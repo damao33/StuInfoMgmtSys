5a3b99c5e01b1d7cf1d16ce2e81045345043de02
zh <zh@192.168.2.180>
2020-06-22 10:28:59 +0800

    1

---
+++ src/cn/sims/controller/StuMgmtController.java
@@ -0,0 +1,47 @@
+package cn.sims.controller;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import cn.sims.util.MyBatisUtil;
+import cn.sims.dao.StudentMapper;
+import cn.sims.model.Student;
+import cn.sims.model.StudentExample;
+
+@Controller
+@RequestMapping("/stuMgmt")
+public class StuMgmtController {
+	private SqlSession sqlSession;
+	private StudentMapper studentDao;
+	List<Student> list;
+	@RequestMapping("/")
+	public ModelAndView toStuMgmt()
+	{
+		ModelAndView modelAndView = new ModelAndView("stuMgmt");
+		return modelAndView;		
+	}
+	@RequestMapping("/insert")
+	public ModelAndView insertUserByAccount(HttpServletRequest request) throws ParseException
+	{
+		sqlSession = MyBatisUtil.getSqlSession();
+		studentDao = sqlSession.getMapper(StudentMapper.class);
+		Map<String, String[]>map=request.getParameterMap();
+		Student student = new Student();
+
+		int num = studentDao.insert(student);
+		sqlSession.commit();
+		ModelAndView modelAndView = new ModelAndView("stuMgmt");
+		modelAndView.addObject("num", num);
+		return modelAndView;
+	}
+	
+}
