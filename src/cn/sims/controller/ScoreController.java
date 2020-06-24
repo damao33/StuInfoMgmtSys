0fa41135b6ac7cb4369af273ef90ef2dcd31a5f3
Zhao <744656354@qq.com>
2020-06-24 11:00:26 +0800

    增加了判断

--- src/cn/sims/controller/ScoreController.java
+++ src/cn/sims/controller/ScoreController.java
@@ -20,75 +20,80 @@ import cn.sims.util.MyBatisUtil;
 @Controller
 @RequestMapping("/scoreInfo")
 public class ScoreController {
-	
+
 	private SqlSession sqlSession;
 	private ScoreMapper scoreDao;
 	List<Score> list;
-	
-	@RequestMapping("/")//查询所有
-	public ModelAndView toscoreMgmt(HttpServletRequest request)
-	{
+
+	@RequestMapping("/") // 查询所有
+	public ModelAndView toscoreMgmt(HttpServletRequest request) {
 		int currentPage;
-		String cPage = request.getParameter("currentPage");//获取request传来的当前页面
-		if(cPage==null||cPage.equals("")||cPage.equals("0"))currentPage=1;//如果当前页面不合法则设为1
-		else currentPage = Integer.parseInt(cPage);
-		PageHelper.startPage(currentPage, 7);//查询第几页，每页7条记录
-		
+		String cPage = request.getParameter("currentPage");// 获取request传来的当前页面
+		if (cPage == null || cPage.equals("") || cPage.equals("0"))
+			currentPage = 1;// 如果当前页面不合法则设为1
+		else
+			currentPage = Integer.parseInt(cPage);
+		PageHelper.startPage(currentPage, 7);// 查询第几页，每页7条记录
+
 		sqlSession = MyBatisUtil.getSqlSession();
 		scoreDao = sqlSession.getMapper(ScoreMapper.class);
 		ScoreExample se = new ScoreExample();
 		list = scoreDao.selectByExample(se);
-		PageInfo<Score> page = new PageInfo<>(list);//根据查询得到的list来用插件生成PageInfo页面信息
+		PageInfo<Score> page = new PageInfo<>(list);// 根据查询得到的list来用插件生成PageInfo页面信息
 		ModelAndView modelAndView = new ModelAndView("scoreInfo");
-		modelAndView.addObject("mapname", "/");//传回映射名
-		modelAndView.addObject("scorelist", page);//传回插件生成的页面信息PageInfo
+		modelAndView.addObject("mapname", "/");// 传回映射名
+		modelAndView.addObject("scorelist", page);// 传回插件生成的页面信息PageInfo
 		MyBatisUtil.closeSqlSession();
 		return modelAndView;
 	}
+
 	@RequestMapping("/cno")
-	public ModelAndView selectScoreByCno(HttpServletRequest request)
-	{
+	public ModelAndView selectScoreByCno(HttpServletRequest request) {
 		int currentPage;
-		String cPage = request.getParameter("currentPage");//获取request传来的当前页面
-		if(cPage==null||cPage.equals("")||cPage.equals("0"))currentPage=1;//如果当前页面不合法则设为1
-		else currentPage = Integer.parseInt(cPage);
-		PageHelper.startPage(currentPage, 7);//查询第几页，每页7条记录
+		String cPage = request.getParameter("currentPage");// 获取request传来的当前页面
+		if (cPage == null || cPage.equals("") || cPage.equals("0"))
+			currentPage = 1;// 如果当前页面不合法则设为1
+		else
+			currentPage = Integer.parseInt(cPage);
+		PageHelper.startPage(currentPage, 7);// 查询第几页，每页7条记录
 		sqlSession = MyBatisUtil.getSqlSession();
 		scoreDao = sqlSession.getMapper(ScoreMapper.class);
-		String  cno = request.getParameter("cno");
+		String cno = request.getParameter("cno");
 		ScoreExample se = new ScoreExample();
 		ScoreExample.Criteria c = se.createCriteria();
 		c.andCnoEqualTo(cno);
 		list = scoreDao.selectByExample(se);
 		ModelAndView modelAndView = new ModelAndView("scoreInfo");
-		PageInfo<Score> page = new PageInfo<>(list);//根据查询得到的list来用插件生成PageInfo页面信息
-		modelAndView.addObject("mapname", "/cno");//传回映射名
-		modelAndView.addObject("scorelist", page);//传回插件生成的页面信息PageInfo
-		modelAndView.addObject("attributeType","&cno=");
+		PageInfo<Score> page = new PageInfo<>(list);// 根据查询得到的list来用插件生成PageInfo页面信息
+		modelAndView.addObject("mapname", "/cno");// 传回映射名
+		modelAndView.addObject("scorelist", page);// 传回插件生成的页面信息PageInfo
+		modelAndView.addObject("attributeType", "&cno=");
 		modelAndView.addObject("attributeValue", cno);
 		MyBatisUtil.closeSqlSession();
 		return modelAndView;
 	}
+
 	@RequestMapping("/sno")
-	public ModelAndView selectScoreBySno(HttpServletRequest request)
-	{
+	public ModelAndView selectScoreBySno(HttpServletRequest request) {
 		int currentPage;
-		String cPage = request.getParameter("currentPage");//获取request传来的当前页面
-		if(cPage==null||cPage.equals("")||cPage.equals("0"))currentPage=1;//如果当前页面不合法则设为1
-		else currentPage = Integer.parseInt(cPage);
-		PageHelper.startPage(currentPage, 7);//查询第几页，每页7条记录
+		String cPage = request.getParameter("currentPage");// 获取request传来的当前页面
+		if (cPage == null || cPage.equals("") || cPage.equals("0"))
+			currentPage = 1;// 如果当前页面不合法则设为1
+		else
+			currentPage = Integer.parseInt(cPage);
+		PageHelper.startPage(currentPage, 7);// 查询第几页，每页7条记录
 		sqlSession = MyBatisUtil.getSqlSession();
 		scoreDao = sqlSession.getMapper(ScoreMapper.class);
-		String  sno = request.getParameter("sno");
+		String sno = request.getParameter("sno");
 		ScoreExample se = new ScoreExample();
 		ScoreExample.Criteria c = se.createCriteria();
 		c.andSnoEqualTo(sno);
 		list = scoreDao.selectByExample(se);
 		ModelAndView modelAndView = new ModelAndView("scoreInfo");
-		PageInfo<Score> page = new PageInfo<>(list);//根据查询得到的list来用插件生成PageInfo页面信息
-		modelAndView.addObject("mapname", "/sno");//传回映射名
-		modelAndView.addObject("scorelist", page);//传回插件生成的页面信息PageInfo
-		modelAndView.addObject("attributeType","&sno=");
+		PageInfo<Score> page = new PageInfo<>(list);// 根据查询得到的list来用插件生成PageInfo页面信息
+		modelAndView.addObject("mapname", "/sno");// 传回映射名
+		modelAndView.addObject("scorelist", page);// 传回插件生成的页面信息PageInfo
+		modelAndView.addObject("attributeType", "&sno=");
 		modelAndView.addObject("attributeValue", sno);
 		MyBatisUtil.closeSqlSession();
 		return modelAndView;
